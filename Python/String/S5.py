e0bebeeb8ac63cc1d5a753d292f45423802c0c6d
Sauraf Kumar <saurafsps7631@gmail.com>
2026-04-30 11:36:44 +0530

    String

---
+++ Python/String/S5.py
@@ -0,0 +1,16 @@
+# Length Of Last Word
+def length_of_last_word(s):
+    i = len(s)-1
+    counter = 0
+    
+    while s[i] == " ":
+        i-=1
+    
+    while s[i]!=" ":
+        counter += 1
+        i -= 1
+    return counter;
+
+
+result = length_of_last_word(" Python is sssimple     ")
+print(f"{result}")
