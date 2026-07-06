3e2f54bc7c7c97b246f2eea581f6c94d9d3de1ec
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-06 11:57:10 +0530

    RestAPI/TODO

---
+++ RestAPI/TODO/src/repository/todo_repository.py
@@ -0,0 +1,23 @@
+from sqlalchemy import select
+
+from src.models import ToDo
+
+
+class ToDoRepository:
+    def __init__(self,session):
+        self.session = session
+
+    async def create(self,todo:ToDo):
+        self.session.add(todo)
+        await self.session.flush()
+        await self.session.refresh(todo)
+        return todo
+
+    async def fetch_all(self):
+        statement = select(ToDo)
+        result = await self.session.execute(statement)
+        return result.scalars().all()
+
+    async def fetch_todo_by_id(self,id:int):
+        return await self.session.get(ToDo,id)
+
---
+++ RestAPI/TODO/src/service/todo_service.py
@@ -0,0 +1,21 @@
+from src.models import ToDo
+from src.repository.todo_repository import ToDoRepository
+from src.schema.todo_schema import ToDoRequest
+
+
+class ToDoService:
+    def __init__(self,session,todo_repo:ToDoRepository):
+        self.session = session
+        self.todo_repo = todo_repo
+
+    async def create(self,request:ToDoRequest):
+        todo = ToDo(title = request.title,description=request.description,priority = request.priority)
+        return await self.todo_repo.create(todo)
+
+    async def fetch_all(self):
+        return await self.todo_repo.fetch_all()
+
+    async def fetch_todo_by_id(self,id:int):
+        todo = await self.todo_repo.fetch_todo_by_id(id)
+        return todo
+
