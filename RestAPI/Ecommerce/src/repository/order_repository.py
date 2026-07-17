56502e157dc2707d7860305128bbb2ac49ab29b4
Sauraf Kumar <saurafsps7631@gmail.com>
2026-07-17 21:49:55 +0530

    Update

--- RestAPI/Ecommerce/src/repository/order_repository.py
+++ RestAPI/Ecommerce/src/repository/order_repository.py
@@ -1,8 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.models import Order
+
 
 class OrderRepository:
     def __init__(self,session:AsyncSession):
         self.session = session
 
-    
+    async def create_order(self,order:Order):
+        pass
